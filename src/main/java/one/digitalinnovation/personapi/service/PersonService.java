5f2f83a48f6debee9839f5e1457f6ba9b9a3c08c
Caio Matheus de Proença <cmproenca2@gmail.com>
2021-07-19 13:24:53 -0300

    Get all persons implemented.

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -8,12 +8,15 @@ import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Service
 public class PersonService {
     private PersonRepository personRepository;
 
-    private final PersonMapper personMapper = PersonMapper.INSTANCE
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
     @Autowired
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
@@ -24,5 +27,10 @@ public class PersonService {
         Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO.builder().message("Created person with ID " + savedPerson.getId()).
                 build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream().map(personMapper::toDTO).collect(Collectors.toList());
     }
 }
