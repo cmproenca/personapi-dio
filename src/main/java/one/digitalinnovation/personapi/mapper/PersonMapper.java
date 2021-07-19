f6a893fb639e99d44bf27172ef805f742c3202af
Caio Matheus de Proença <cmproenca2@gmail.com>
2021-07-19 13:59:00 -0300

    Get person by Id implemented.

--- src/main/java/one/digitalinnovation/personapi/mapper/PersonMapper.java
+++ src/main/java/one/digitalinnovation/personapi/mapper/PersonMapper.java
@@ -6,12 +6,12 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface PersonMapper {
     PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
 
     @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
-    Person toModel (PersonDTO personDTO);
+    Person toModel(PersonDTO personDTO);
 
     PersonDTO toDTO(Person person);
 }
